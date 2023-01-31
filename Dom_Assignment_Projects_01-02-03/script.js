dbd6cb22741a6d4f361ee02bb54f9a92d68349be
anilbhangay <anilbhangay@gmail.com>
2023-01-31 23:47:48 +0530

    Added javascript file

---
+++ Dom_Assignment_Projects_01-02-03/script.js
@@ -0,0 +1,9 @@
+ 
+ //A new element in the navigation menu.
+let list = document.createElement("li");
+list.innerHTML = "<a>Hire Me</a>";
+let ul = document.querySelector("ul");
+ul.appendChild(list);
+
+
+//Change the placeholder message.
