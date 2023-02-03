b70c78d9f0a0610c7cd5a07ef6714dc79cec7c13
anilbhangay <anilbhangay@gmail.com>
2023-02-03 19:33:24 +0530

    added color to head and para

--- Dom_Assignment_Projects_01-02-03/About/about.js
+++ Dom_Assignment_Projects_01-02-03/About/about.js
@@ -12,13 +12,48 @@ if (para.style.display === "block") {
 });
 
 
-// Change the color of heading and paragraph.
+// // Change the color of heading and paragraph.
 let heading = document.querySelectorAll(".accordian h3");
 let parag = document.querySelectorAll(".accordian p");
 heading.forEach((element)=> {element.style.backgroundColor="#dadaf8"});
 parag.forEach((element)=> {element.style.backgroundColor="#eeeeff"});
 
 
+
 // Adding new accordian to the page.
 let parent = document.querySelector(".accordian-wrapper");
+let element = document.createElement("div");
+element.classList.add("accordian");
+let head = document.createElement("h3");
+let para = document.createElement("p");
+para.innerText = "I posses a very good command over the Full Stack Developement technologies like MERN which can be seen in my  work over the Github.";
+para.style.display = "none";
+head.innerText = "Skills";
+element.appendChild(head);
+element.appendChild(para);
+parent.appendChild(element);
+
+
+// Adding code for addEventListener.
+let newList = document.querySelectorAll(".accordian h3");
+newList[newList.length-1].addEventListener("click", (event) => {
+    let element = event.target.nextElementSibling;
+    if (element.style.display === "block") {
+            element.style.display = "none";
+    } else {
+            element.style.display = "block";
+    }
+});
+
+
+//  Change the color of heading and paragraph of last div.
+let heaad = document.querySelectorAll(".accordian h3");
+let paara = document.querySelectorAll(".accordian p");
+heaad.forEach((element)=> {element.style.backgroundColor="#dadaf8"});
+paara.forEach((element)=> {element.style.backgroundColor="#eeeeff"});
+
+
+
+
+
 
