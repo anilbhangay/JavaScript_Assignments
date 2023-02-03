50084c317d30b3cdaa1d00f4a855a5efe048348e
anilbhangay <anilbhangay@gmail.com>
2023-02-03 20:01:36 +0530

    Added data value in contact js

---
+++ Dom_Assignment_Projects_01-02-03/Contact/contact.js
@@ -0,0 +1,19 @@
+// Added addEventListener to second form.
+let myform = document.querySelectorAll("form");
+myform[1].addEventListener("submit", (event)=> {
+        event.preventDefault();
+
+     //getting entered data
+      let name = document.querySelector(".userName");
+      let email = document.querySelector(".userEmail");
+      let message = document.querySelector(".userMessage");
+     
+      //setting enterd data
+      let setName = document.querySelector(".enterName");
+      let setEmail = document.querySelector(".enterMail");
+      let setMessage = document.querySelector(".enterMessage");
+
+      setName.value = name.value;
+      setEmail.value = email.value;
+      setMessage.value = message.value;
+})
