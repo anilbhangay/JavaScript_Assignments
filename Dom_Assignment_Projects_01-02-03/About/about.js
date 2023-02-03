12c10d115997ab80aac96b1a88f15da9ffa8a429
anilbhangay <anilbhangay@gmail.com>
2023-02-03 11:33:40 +0530

    Added About page and Contact page

--- Dom_Assignment_Projects_01-02-03/About/about.js
+++ Dom_Assignment_Projects_01-02-03/About/about.js
@@ -10,3 +10,15 @@ if (para.style.display === "block") {
   }
  });
 });
+
+
+// Change the color of heading and paragraph.
+let heading = document.querySelectorAll(".accordian h3");
+let parag = document.querySelectorAll(".accordian p");
+heading.forEach((element)=> {element.style.backgroundColor="#dadaf8"});
+parag.forEach((element)=> {element.style.backgroundColor="#eeeeff"});
+
+
+// Adding new accordian to the page.
+let parent = document.querySelector(".accordian-wrapper");
+
