c290bfe3fe9af6b6f41cc7e5cb8828bdd956a37d
anilbhangay <anilbhangay@gmail.com>
2023-02-09 19:49:39 +0530

    code for adding color

---
+++ Dom_Assignment_Project_04/script.js
@@ -0,0 +1,10 @@
+  //Changing background color of barbarian clash card
+let color = document.querySelector(".clash-card__unit-stats--barbarian");
+color.style.backgroundColor = "#ec9b3b";
+color.style.color = "white";
+
+var last = document.querySelector(".clash-card__unit-stats--barbarian .no-border").style.color = "white";
+
+
+ //Changing background color of archer clash card
+  
