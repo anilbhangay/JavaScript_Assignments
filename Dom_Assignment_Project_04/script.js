d870360ea89bc1536ee6c03a1333e607e8d16d6a
anilbhangay <anilbhangay@gmail.com>
2023-02-09 22:42:32 +0530

    Added background color with javascript

--- Dom_Assignment_Project_04/script.js
+++ Dom_Assignment_Project_04/script.js
@@ -7,4 +7,37 @@ var last = document.querySelector(".clash-card__unit-stats--barbarian .no-border
 
 
  //Changing background color of archer clash card
-  
+ let paint = document.querySelector(".clash-card__unit-stats--archer");
+ paint.style.backgroundColor = "#ee5487";
+ paint.style.color = "white";
+
+ var ctc = document.querySelector(".clash-card__unit-stats--archer .no-border").style.color = "white";
+
+
+ //Changing background color of giant clash card 
+ var post = document.querySelector(".clash-card__unit-stats--giant");
+ post.style.backgroundColor = "#f6901a";
+ post.style.color = "white";
+
+ var srt = document.querySelector(".clash-card__unit-stats--giant .no-border").style.color = "white";
+
+
+ //Changing background color of goblin clash card
+ let pok = document.querySelector(".clash-card__unit-stats--goblin");
+ pok.style.backgroundColor = "#82bb30";
+ pok.style.color = "white";
+
+ var ppk = document.querySelector(".clash-card__unit-stats--goblin .no-border").style.color = "white";
+
+
+ //Changing background color of wizard clash card
+ let jk = document.querySelector(".clash-card__unit-stats--wizard");
+ jk.style.backgroundColor = "#4facff";
+ jk.style.color = "white";
+
+ var kkk = document.querySelector(".clash-card__unit-stats--wizard .no-border").style.color = "white";
+
+
+ //Changing all clash cards in flex 
+ let doc = document.querySelector(".container-all");
+ doc.style.display =  "flex";
