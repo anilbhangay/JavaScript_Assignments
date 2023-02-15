ab99e9141a9434180c0bb32f3718ddee3e3176ea
anilbhangay <anilbhangay@gmail.com>
2023-02-16 01:17:23 +0530

    Created weather api with Dom

--- Weather_API_Project/script.js
+++ Weather_API_Project/script.js
@@ -9,7 +9,7 @@ let temprature = document.getElementById("temprature");
 let logoImage = document.getElementById("logoImage");
 let weatherStatus = document.getElementById("weatherStatus");
 
-const getData = (event) => {
+const getData = async (event) => {
      event.preventDefault();
      if(!inputBox.value) {
          alert("Please Enter the city name: ");
@@ -17,4 +17,20 @@ const getData = (event) => {
      }
 
      const city = inputBox.value;
+     //Fetch Data
+     const fetchData = await fetch(`http://api.weatherapi.com/v1/current.json?key=6aa7799b0dce4436932125152231502&q=${city}`);
+
+     const orgData = await fetchData.json();
+     data = orgData;
+     console.log(data);
+
+     //Displaying the data in html
+     countryName.innerHTML = data.location.country;
+     stateName.innerHTML = data.location.region;
+     cityName.innerHTML = data.location.name;
+     humidityName.innerHTML = data.current.humidity;
+     windSpeed.innerHTML = data.current.wind_kph;
+     temprature.innerHTML = data.current.temp_c;
+     logoImage.src = data.current.condition.icon;
+     weatherStatus.innerHTML = data.current.condition.text;
 };
