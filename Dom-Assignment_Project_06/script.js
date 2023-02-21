2959de6047aac8977a635dfec5da3ae3a15b03dd
anilbhangay <anilbhangay@gmail.com>
2023-02-21 18:56:25 +0530

    Added DOM in all files

---
+++ Dom-Assignment_Project_06/script.js
@@ -0,0 +1,9 @@
+// Changing top logo to ineuron logo
+
+let list = document.querySelector('main header img');
+list.src="./assets/ineuron-logo.png";
+
+let color = document.querySelector('body');
+color.style.backgroundImage = url("./assets/bg-main-tablet.png");
+
+// changing price of
